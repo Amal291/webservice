08702d69f1030bfbc37f54871cbdb1d601e6c36b
Amal <amalboudra291@gmail.com>
2023-10-14 17:24:32 +0200

    Initial commit

---
+++ src/main/java/ws/ServerWS.java
@@ -0,0 +1,11 @@
+package ws;
+
+
+import jakarta.xml.ws.Endpoint;
+
+public class ServerWS {
+    public static void main(String[] args){
+    Endpoint.publish("http://0.0.0.0:9191/", new BanqueService());
+    System.out.println("web service deploye sur http://0.0.0.0:9191");
+}
+}
